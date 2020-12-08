4ffa65d61eeeec2f0006803869084b588dfca0a9
EnergyNick <nsikachin@gmail.com>
2020-12-08 20:31:07 +0500

    Rework of IMethodRegistrationManager that remove in-time method registration

---
+++ ExtendedHubClient.Abstractions/Methods/IMethodsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace ExtendedHubClient.Abstractions.Methods
+{
+    /// <summary>
+    /// Provides an interface for easy display of registration results and checking for any method.
+    /// </summary>
+    public interface IMethodsManager
+    {
+        IReadOnlyCollection<MethodView> SendMethods { get; }
+        
+        IReadOnlyCollection<MethodView> ReceiveMethods { get; }
+
+        /// <summary>
+        /// Allows you to find out if a similar method exists in registration.
+        /// </summary>
+        /// <param name="type">Method type in registration</param>
+        /// <param name="name">Method name</param>
+        /// <param name="arguments">Method arguments</param>
+        /// <param name="errorReason">Reason for the negative result</param>
+        bool IsMethodContainsInRegistration(MethodType type, 
+            string name, 
+            IReadOnlyList<object> arguments,
+            out string errorReason);
+        
+        
+        /// <param name="type">Method type in registration</param>
+        /// <param name="name">Method name</param>
+        /// <param name="arguments">Method arguments</param>
+        /// <param name="methodView">Method if the search is successful</param>
+        /// <returns></returns>
+        bool TryGetMethodFromRegistration(MethodType type, 
+            string name, 
+            IReadOnlyList<object> arguments,
+            out MethodView methodView);
+    }
+}
--- ExtendedHubClient.Abstractions/Methods/MethodView.cs
+++ ExtendedHubClient.Abstractions/Methods/MethodView.cs
@@ -5,20 +5,16 @@ namespace ExtendedHubClient.Abstractions.Methods
 {
     public class MethodView : IEquatable<MethodView>
     {
+        public string Name { get; }
+        public Type[] Arguments { get; }
+        public Type ReturnValue { get; }
+
         public MethodView(string name, Type[] arguments, Type returnValue = null)
         {
             Name = name;
             Arguments = arguments;
             ReturnValue = returnValue ?? typeof(Task);
         }
-        
-        public MethodView(MethodViewDto methodViewDto)
-        : this(methodViewDto.Name, methodViewDto.Arguments, methodViewDto.ReturnValue)
-        { }
-
-        public string Name { get; }
-        public Type[] Arguments { get; }
-        public Type ReturnValue { get; }
 
         public bool Equals(MethodView other)
         {
