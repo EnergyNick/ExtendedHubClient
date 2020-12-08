919c7e37cff8028539a956acf49a1372fae5a556
EnergyNick <nsikachin@gmail.com>
2020-12-08 20:29:35 +0500

    Create ITypedHubClient for more simple and fluent access to call server methods

---
+++ ExtendedHubClient.Abstractions/ITypedHubClient.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtendedHubClient.Abstractions
+{
+    /// <summary>
+    /// Interface of strongly typed SignalR client.
+    /// </summary>
+    /// <typeparam name="TServerMethods">The type of client.</typeparam>
+    public interface ITypedHubClient<out TServerMethods> : IHubClient
+        where TServerMethods: class
+    {
+        /// <summary>
+        /// Interface for calling server methods.
+        /// </summary>
+        new TServerMethods Server { get; }
+    }
+}
