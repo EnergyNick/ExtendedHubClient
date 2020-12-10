5a140995d90b8e82b87737ba9da614402f2d6280
EnergyNick <nsikachin@gmail.com>
2020-12-10 19:10:25 +0500

    Rework names and method invocations based on changes in ExtendedHubClient.Abstractions.

--- ExtendedHubClient/Proxy/Interceptors/MethodProxyInterceptorWrapper.cs
+++ ExtendedHubClient/Proxy/Interceptors/MethodProxyInterceptorWrapper.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using Castle.DynamicProxy;
+using ExtendedHubClient.Abstractions;
 using ExtendedHubClient.Abstractions.Proxy;
 
 namespace ExtendedHubClient.Proxy.Interceptors
 {
-    internal class MethodProxyInterceptorWrapper : IInterceptorWrapper
+    public class MethodProxyInterceptorWrapper : IInterceptorWrapper
     {
-        private IMethodHolder _holder;
+        private IMethodProxy _methodProxy;
         
         private readonly object _locker = new object();
 
@@ -15,28 +16,28 @@ namespace ExtendedHubClient.Proxy.Interceptors
             if(invocation == null)
                 throw new ArgumentNullException(nameof(invocation));
             
-            if(_holder == null)
-                throw new NullReferenceException($"Can't invoke without attached {nameof(IMethodHolder)}");
+            if(_methodProxy == null)
+                throw new NullReferenceException($"Can't invoke without attached {nameof(IMethodProxy)}");
 
             if (invocation.Arguments.Length != 2
                 || !(invocation.Arguments[0] is string)
                 || !(invocation.Arguments[1] is object[]))
                 throw new InvalidOperationException(
-                    $"{nameof(MethodProxyInterceptorWrapper)} can only work with {nameof(ISendMethodProxy)}");
+                    $"{nameof(MethodProxyInterceptorWrapper)} can only work with {nameof(IHubClient)}");
             
             lock (_locker)
             {
                 var name = invocation.Arguments[0] as string;
                 var arguments = invocation.Arguments[1] as object[];
-                _holder.OnMethodInvoke(name, arguments).GetAwaiter().GetResult();
+                invocation.ReturnValue = _methodProxy.OnMethodInvoke(name, arguments);
             }
         }
 
-        public void AttachMethodHolder(IMethodHolder holder)
+        public void AttachMethodHolder(IMethodProxy holder)
         {
             lock (_locker)
             {
-                _holder = holder ?? throw new ArgumentNullException(nameof(holder));
+                _methodProxy = holder ?? throw new ArgumentNullException(nameof(holder));
             }
         }
     }
